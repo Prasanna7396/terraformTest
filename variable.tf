eb2f41a7013a85e0b078174b021f5ce53fc03817
Prasanna7396 <41993582+Prasanna7396@users.noreply.github.com>
2022-04-26 19:16:43 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,8 +7,14 @@ variable "AWS_REGION" {
   default = "us-east-1"
 }
 
-variable "AWS_ACCESS_KEY" {}
+variable "AWS_ACCESS_KEY" {
+  default = "AKIATI666CD3TXNH7YX3"
+  type = string
+}
 
-variable "AWS_SECRET_KEY" {}
+variable "AWS_SECRET_KEY" {
+  default = "Ju9iTvFnfYdSPX6TAt+zxL5i+mEuDEZ83o+gJ6Bo"
+  type = string
+}
 
 
